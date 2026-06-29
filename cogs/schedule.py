839aef93616db0500f812731f4a095ea1b0419c1
korail0316 <81457641+korail0316@users.noreply.github.com>
2026-06-29 13:45:11 +0900

    Update schedule.py

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -1,4 +1,11 @@
+import discord
 from discord.ext import commands
 
+class Schedule(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # 여기에 캘린더/스케줄 관련 기능을 넣을 예정
+
 async def setup(bot):
-    pass # 아무것도 하지 않고 로드만 성공시킴
+    await bot.add_cog(Schedule(bot))
